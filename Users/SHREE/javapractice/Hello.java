4fdcc73ffb4a6db265b3a81144fe811dd1eb6f5b
technology-97 <rushikeshbhandare789@gmail.com>
2021-07-10 09:21:26 +0600

    Added:UC1 program is added

--- Users/SHREE/javapractice/Hello.java
+++ Users/SHREE/javapractice/Hello.java
@@ -5,6 +5,14 @@ public class Hello
 {
 
     System.out.println("Hello");
-}
+ int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			 System.out.println("Employee is Absent");
+	}
 
 }
+
+
