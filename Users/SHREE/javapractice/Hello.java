fba29ad82ada436e669a1faa7ee806027401fe62
technology-97 <rushikeshbhandare789@gmail.com>
2021-07-10 09:44:09 +0600

    Added:UC2 is added

--- Users/SHREE/javapractice/Hello.java
+++ Users/SHREE/javapractice/Hello.java
@@ -5,12 +5,21 @@ public class Hello
 {
 
     System.out.println("Hello");
+   int wagePerHour = 20;
+   int fullDayHour = 8;
+   int dailyWage = 0;
+
+
  int IS_FULL_TIME = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			System.out.println("Employee is Present");
 		else
 			 System.out.println("Employee is Absent");
+
+                
+
+
 	}
 
 }
